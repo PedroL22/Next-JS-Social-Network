f439718336393f2624c4c3268587d3e280b90996
Pedro L <pedrocontact22@gmail.com>
2022-11-18 15:27:35 -0300

    NavBar component + account page

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,57 +2,57 @@ import Link from "next/link";
 import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function NavBar() {
-	const { data: session } = useSession();
+  const { data: session } = useSession();
 
-	if (session) {
-		return (
-			<nav className="fixed h-16 bg-red-700 mb-10 w-screen">
-				<div className="flex mx-auto max-w-7xl justify-between p-5 w-screen">
-					<Link href="/">
-						<h1 className="text-white font-medium cursor-pointer hover:text-gray-200 transition-all ease-in duration-75">
-							The News Website
-						</h1>
-					</Link>
-					<div className="flex">
-						<Link href="account">
-							<img
-								src={session.user.image}
-								alt="user profile picture"
-								className="w-12 rounded-full my-auto mx-2 -mt-3 cursor-pointer"
-							/>
-						</Link>
-						<Link href="account">
-							<p className="xl:flex hidden text-white font-medium cursor-pointer my-auto mx-2 -mt-0 hover:text-gray-200 transition-all ease-in duration-75">
-								{session.user.name}
-							</p>
-						</Link>
-						<button
-							onClick={signOut}
-							className="text-white font-medium cursor-pointer my-auto mx-2 -mt-0 hover:text-gray-200 transition-all ease-in duration-75"
-						>
-							Logout
-						</button>
-					</div>
-				</div>
-			</nav>
-		);
-	} else {
-		return (
-			<nav className="fixed h-16 bg-red-700 mb-10 w-screen">
-				<div className="flex mx-auto max-w-7xl justify-between p-5 w-screen">
-					<Link href="/">
-						<h1 className="text-white font-medium cursor-pointer hover:text-gray-200 transition-all ease-in duration-75">
-							The News Website
-						</h1>
-					</Link>
-					<button
-						onClick={signIn}
-						className="text-white font-medium cursor-pointer hover:text-gray-200 transition-all ease-in duration-75"
-					>
-						Login
-					</button>
-				</div>
-			</nav>
-		);
-	}
+  if (session) {
+    return (
+      <nav className="fixed h-16 bg-blue-700 mb-10 w-screen">
+        <div className="flex mx-auto max-w-7xl justify-between p-5 w-screen">
+          <Link href="/">
+            <h1 className="text-white font-medium cursor-pointer hover:text-gray-200 transition-all ease-in duration-75">
+              Next JS Social Network
+            </h1>
+          </Link>
+          <div className="flex">
+            <Link href="account">
+              <img
+                src={session.user.image}
+                alt="user profile picture"
+                className="w-12 rounded-full my-auto mx-2 -mt-3 cursor-pointer"
+              />
+            </Link>
+            <Link href="account">
+              <p className="xl:flex hidden text-white font-medium cursor-pointer my-auto mx-2 -mt-0 hover:text-gray-200 transition-all ease-in duration-75">
+                {session.user.name}
+              </p>
+            </Link>
+            <button
+              onClick={signOut}
+              className="text-white font-medium cursor-pointer my-auto mx-2 -mt-0 hover:text-gray-200 transition-all ease-in duration-75"
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      </nav>
+    );
+  } else {
+    return (
+      <nav className="fixed h-16 bg-red-700 mb-10 w-screen">
+        <div className="flex mx-auto max-w-7xl justify-between p-5 w-screen">
+          <Link href="/">
+            <h1 className="text-white font-medium cursor-pointer hover:text-gray-200 transition-all ease-in duration-75">
+              The News Website
+            </h1>
+          </Link>
+          <button
+            onClick={signIn}
+            className="text-white font-medium cursor-pointer hover:text-gray-200 transition-all ease-in duration-75"
+          >
+            Login
+          </button>
+        </div>
+      </nav>
+    );
+  }
 }
---
+++ pages/account.tsx
@@ -0,0 +1,60 @@
+import Head from "next/head";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
+
+export default function account() {
+  const { data: session } = useSession({ required: true });
+
+  if (session) {
+    return (
+      <div>
+        <Head>
+          <title>{session.user.name} - The News Website</title>
+          <meta
+            name="description"
+            content="A news website made with The Guardian API, Next JS, and Tailwind CSS based on Globo's G1."
+          />
+          <link rel="icon" href="/favicon.png" />
+        </Head>
+
+        <div className="xl:pt-36 pt-20 xl:ml-5 xl:flex block">
+          <img
+            src={session.user.image}
+            alt="user profile picture"
+            className="w-36 xl:mx-0 mx-auto rounded-lg"
+          />
+          <div>
+            <h1 className="xl:ml-10 mt-2 text-center font-medium text-3xl text-gray-600">
+              {session.user.name}
+            </h1>
+            <p className="xl:ml-10 text-center text-gray-400">
+              {session.user.email}
+            </p>
+            <Link href="/">
+              <button className="flex mt-2 mx-auto xl:ml-24 text-white bg-blue-700 rounded-md px-5 py-2 hover:bg-blue-800 transition-all ease-in duration-75">
+                Back
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Head>
+          <title>The News Website</title>
+          <meta
+            name="description"
+            content="A news website made with The Guardian API, Next JS, and Tailwind CSS based on Globo's G1."
+          />
+          <link rel="icon" href="/favicon.png" />
+        </Head>
+
+        <div className="pt-20 ml-5">
+          <p>...</p>
+        </div>
+      </div>
+    );
+  }
+}
