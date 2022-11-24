3ba5042b0de8022970514e9f1d200b5055c18429
Pedro L <pedrocontact22@gmail.com>
2022-11-24 01:16:53 -0300

    Update axios.ts

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`,
+  baseURL: `https://${process.env.VERCEL_URL}`,
 });
 
 export default api;
